7507b21fd8dd2a2ac8be928da6302f4c2560e0a9
Ales Kobr <Ales_Kobr@derivco.co.uk>
2018-11-19 17:55:02 +0000

    new code base search command project

---
+++ Commands/Commands.CodeBaseSearch/Model/ISubject.cs
@@ -0,0 +1,13 @@
+﻿namespace Commands.CodeBaseSearch.Model
+{
+    public interface ISubject
+    {
+        string Title { get; }
+
+        string Path { get; }
+
+        SubjectTypeEnum Type { get; }
+
+
+    }
+}
