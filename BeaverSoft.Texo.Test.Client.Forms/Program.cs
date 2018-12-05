fc0b04722b8db6110e26a06eee43c258368a8766
Ales Kobr <kobr.ales@hotmail.cz>
2018-12-05 01:03:42 +0000

    project with test client for windows forms test project for powershell in windows forms

---
+++ BeaverSoft.Texo.Test.Client.Forms/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Windows.Forms;
+using BeaverSoft.Texo.Core;
+using BeaverSoft.Texo.Core.Commands;
+using BeaverSoft.Texo.Core.Runtime;
+using BeaverSoft.Texo.Core.View;
+using BeaverSoft.Texo.Test.Client.Forms.Startup;
+using StrongBeaver.Core.Container;
+
+namespace BeaverSoft.Texo.Test.Client.Forms
+{
+    static class Program
+    {
+        public static TexoEngine TexoEngine { get; private set; }
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            StartUp();
+            Application.ApplicationExit += OnApplicationExit;
+            Application.Run(new MainForm());
+        }
+
+        private static void OnApplicationExit(object sender, EventArgs e)
+        {
+            TexoEngine?.Dispose();
+            TexoEngine = null;
+        }
+
+        private static void StartUp()
+        {
+            SimpleIoc container = new SimpleIoc();
+            container.RegisterServices();
+
+            TexoEngineBuilder engineBuilder = new TexoEngineBuilder();
+            container.RegisterEngineServices(engineBuilder);
+
+            CommandFactory commandFactory = new CommandFactory();
+            commandFactory.RegisterCommands(container);
+            container.RegisterCommandFactory(commandFactory);
+
+            engineBuilder.WithFallbackService(container.GetInstance<IFallbackService>());
+            TexoEngine = engineBuilder.Build(commandFactory, container.GetInstance<IViewService>());
+            TexoEngine.InitialiseWithCommands();
+            TexoEngine.Start();
+        }
+    }
+}
---
+++ BeaverSoft.Texo.Test.Client.Forms/Startup/TexoEngineConfig.cs
@@ -0,0 +1,22 @@
+﻿using BeaverSoft.Texo.Commands.FileManager;
+using BeaverSoft.Texo.Commands.NugetManager;
+using BeaverSoft.Texo.Core;
+using Commands.CommandLine;
+using Commands.Dir;
+using Commands.ReferenceCheck;
+
+namespace BeaverSoft.Texo.Test.Client.Forms.Startup
+{
+    public static class TexoEngineConfig
+    {
+        public static void InitialiseWithCommands(this TexoEngine engine)
+        {
+            engine.Initialise(
+                ReferenceCheckCommand.BuildConfiguration(),
+                DirCommand.BuildConfiguration(),
+                CommandLineCommand.BuildConfiguration(),
+                FileManagerBuilder.BuildCommand(),
+                NugetManagerBuilder.BuildCommand());
+        }
+    }
+}
