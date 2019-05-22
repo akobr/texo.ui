e83cda6c5fbe90cc191d9782eb5b69bc083f8a30
Ales Kobr <Ales_Kobr@derivco.co.uk>
2019-05-22 16:22:45 +0100

    added actions to fallback output (auto-URLs and git help)

--- BeaverSoft.Texo.Fallback.PowerShell/Transforming/GitOutput.cs
+++ BeaverSoft.Texo.Fallback.PowerShell/Transforming/GitOutput.cs
@@ -2,6 +2,8 @@ using System;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using BeaverSoft.Texo.Core.Actions;
+using BeaverSoft.Texo.Core.Text;
 using BeaverSoft.Texo.Core.Transforming;
 
 namespace BeaverSoft.Texo.Fallback.PowerShell.Transforming
@@ -9,6 +11,7 @@ namespace BeaverSoft.Texo.Fallback.PowerShell.Transforming
     public class GitOutput : ITransformation<OutputModel>
     {
         private readonly Regex progressRegex = new Regex(@"\s\d{1,3}%\s", RegexOptions.Compiled);
+        private readonly Regex commandHelpRegex = new Regex("\"(?<help>git[^\\n\"]*)\"", RegexOptions.Compiled);
 
         public Task<OutputModel> ProcessAsync(OutputModel data)
         {
@@ -35,11 +38,21 @@ namespace BeaverSoft.Texo.Fallback.PowerShell.Transforming
                 }
 
                 data.Flags.Remove(TransformationFlags.GIT_UPDATE_MODE);
-                builder.Append(text);
+                builder.Append(commandHelpRegex.Replace(text, ReplaceHelpWithAction));
             }
 
             data.Output = builder.ToString();
             return Task.FromResult(data);
+        }
+
+        private string ReplaceHelpWithAction(Match match)
+        {
+            AnsiStringBuilder builder = new AnsiStringBuilder();
+            builder.Append("\"");
+            string helpCommand = match.Groups["help"].Value;
+            builder.AppendLink(helpCommand, ActionBuilder.InputSetUri(helpCommand));           
+            builder.Append("\"");
+            return builder.ToString();
         }
     }
 }
--- BeaverSoft.Texo.Fallback.PowerShell/Transforming/OutputAutoUrl.cs
+++ BeaverSoft.Texo.Fallback.PowerShell/Transforming/OutputAutoUrl.cs
@@ -9,7 +9,7 @@ namespace BeaverSoft.Texo.Fallback.PowerShell.Transforming
 {
     public class OutputAutoUrl : ITransformationWithControl<OutputModel>
     {
-        private readonly Regex urlExpression = new Regex("https?:\\/\\/(www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{2,256}\\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_\\+.~#?&//=]*)", RegexOptions.Compiled);
+        private readonly Regex urlExpression = new Regex(@"https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_\+.~#?&\/=]*)", RegexOptions.Compiled);
 
         public async Task<OutputModel> ProcessAsync(OutputModel data, Func<OutputModel, Task<OutputModel>> nextFlow)
         {
