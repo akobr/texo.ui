32e7fe333c2ef81ff618ac103b64697571a7a54c
Ales Kobr <kobr.ales@hotmail.cz>
2020-02-19 02:01:19 +0000

    added VT100 viewer as .NET framework windows forms native project (testing)

--- tools/VT100.Viewer/Program.cs
+++ tools/VT100.Viewer/Program.cs
@@ -14,7 +14,7 @@ namespace VT100.Viewer
         [STAThread]
         static void Main()
         {
-            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            //Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
